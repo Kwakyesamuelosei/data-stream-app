ea8feeda05da78d0707570418eb28c5f50a5f0b1
Samuel Osei Kwakye <kwakyesamuelosei@gmail.com>
2019-12-13 16:36:24 +0000

    Using PollableSource Interface to pull data from Webservice(Twitter API)

--- src/main/java/io/turntabl/Main.java
+++ src/main/java/io/turntabl/Main.java
@@ -4,6 +4,7 @@ import com.google.common.base.Charsets;
 import org.apache.flume.*;
 import org.apache.flume.channel.ChannelProcessor;
 import org.apache.flume.event.EventBuilder;
+import org.apache.flume.source.PollableSourceConstants;
 import twitter4j.Status;
 import twitter4j.User;
 import org.apache.flume.channel.ChannelProcessor;
@@ -22,128 +23,78 @@ import java.util.TimerTask;
 import java.util.stream.Collectors;
 
 
-
-public class Main extends AbstractSource
-        implements EventDrivenSource, Configurable {
+public class Main extends AbstractSource implements PollableSource, Configurable {
 
     MyTwitterController myTwitterController = null;
+    Long backoffSleepIncrement;
+    Long maxBackOffSleepInterval;
 
+    @Override
     public void configure(Context context) {
-         myTwitterController = new MyTwitterController(context);
+         myTwitterController = new MyTwitterController();
+         //Every 11 secs
+        backoffSleepIncrement = context.getLong(PollableSourceConstants.BACKOFF_SLEEP_INCREMENT
+                , (long) 10000);
+        //Every 60 sec
+        maxBackOffSleepInterval = context.getLong(PollableSourceConstants.MAX_BACKOFF_SLEEP
+                , (long) 55000);
     }
 
+    @Override
     public void start() {
-        // The channel is the piece of Flume that sits between the Source and Sink,
-        // and is used to process events.
-        final ChannelProcessor channel = getChannelProcessor();
+        // Initialize the connection to the external client
+    }
 
-
-        List<twitter4j.Status> allTweets = myTwitterController.fetchTweets();
-        List<User> finalLIst = allTweets.stream()
-                .map(user -> user.getUser())
-                .collect(Collectors.toList());
-        List<Event> events = finalLIst.stream()
-                .map(user -> EventBuilder.withBody(String.valueOf(user), Charsets.UTF_8))
-                .collect(Collectors.toList());
-        channel.processEventBatch(events);
-
+    @Override
+    public void stop () {
+        // Disconnect from external client and do any additional cleanup
+        // (e.g. releasing resources or nulling-out field values) ..
 
     }
 
-    public void stop() {
 
-        super.stop();
+    @Override
+    public Status process() throws EventDeliveryException {
+
+        Status status = null;
+
+        try {
+            // This try clause includes whatever Channel/Event operations you want to do
+
+            // Receive new data
+
+            List<JSONObject> allTweets = myTwitterController.fetchTweets();
+
+            List<Event> events = allTweets.stream()
+                    .map(user -> EventBuilder.withBody(String.valueOf(user), Charsets.UTF_8))
+                    .collect(Collectors.toList());
+            getChannelProcessor().processEventBatch(events);
+
+            status = Status.READY;
+        } catch (Throwable t) {
+            // Log exception, handle individual exceptions as needed
+
+            status = Status.BACKOFF;
+
+            // re-throw all Errors
+            if (t instanceof Error) {
+                throw (Error)t;
+            }
+        }
+        return status;
     }
 
-}
+    @Override
+    public long getBackOffSleepIncrement() {
+        System.out.println("Printing getBackOffSleepIncrement | " + backoffSleepIncrement);
+        return backoffSleepIncrement;
+    }
 
-
-//public class Main extends AbstractSource
-//        implements PollableSource, Configurable {
-//
-//    MyTwitterController myTwitterController = null;
-//
-//    @Override
-//    public void configure(Context context) {
-//        MyTwitterController myTwitterController = new MyTwitterController(context);
-//    }
-//
-//    @Override
-//    public void start() {
-//        // Initialize the connection to the external client
-//    }
-//
-//    @Override
-//    public void stop () {
-//        // Disconnect from external client and do any additional cleanup
-//        // (e.g. releasing resources or nulling-out field values) ..
-//        super.stop();
-//    }
-//
-//
-//    @Override
-//    public Status process() throws EventDeliveryException {
-//
-//        Status status = null;
-//
-//        try {
-//            // This try clause includes whatever Channel/Event operations you want to do
-//
-//            // Receive new data
-//            //Timer timer = new Timer();
-//            //TimerTask doAsynchronousTask = new TimerTask() {
-//              //  @Override
-//                //public void run() {
-//
-////                    List<twitter4j.Status> allTweets = myTwitterController.fetchTweets();
-////                    List<User> finalLIst = allTweets.stream()
-////                            .map(status -> status.getUser())
-////                            .collect(Collectors.toList());
-////                    List<Event> events = finalLIst.stream()
-////                            .map(user -> EventBuilder.withBody(String.valueOf(user), Charsets.UTF_8))
-////                            .collect(Collectors.toList());
-////                    getChannelProcessor().processEventBatch(events);
-//
-////                System.out.println("Response from AllTweets in Main | " + finalLIst);
-////
-////                }
-////            };
-//            //Every 2 mina makes call to fetch data from twitter
-//            //timer.schedule(doAsynchronousTask, 0, 120000);
-//
-//            List<twitter4j.Status> allTweets = myTwitterController.fetchTweets();
-//            List<User> finalLIst = allTweets.stream()
-//                    .map(user -> user.getUser())
-//                    .collect(Collectors.toList());
-//            List<Event> events = finalLIst.stream()
-//                    .map(user -> EventBuilder.withBody(String.valueOf(user), Charsets.UTF_8))
-//                    .collect(Collectors.toList());
-//            getChannelProcessor().processEventBatch(events);
-//
-//
-//            status = Status.READY;
-//        } catch (Throwable t) {
-//            // Log exception, handle individual exceptions as needed
-//
-//            status = Status.BACKOFF;
-//
-//            // re-throw all Errors
-//            if (t instanceof Error) {
-//                throw (Error)t;
-//            }
-//        }
-//        return status;
-//    }
-//
-//    @Override
-//    public long getBackOffSleepIncrement() {
-//        return 0;
-//    }
-//
-//    @Override
-//    public long getMaxBackOffSleepInterval() {
-//        return 0;
-//    }
-//};
+    @Override
+    public long getMaxBackOffSleepInterval() {
+        System.out.println("Printing getMaxBackOffSleepInterval | " + maxBackOffSleepInterval);
+        return maxBackOffSleepInterval;
+    }
+};
 
 
--- src/main/java/io/turntabl/MyTwitterController.java
+++ src/main/java/io/turntabl/MyTwitterController.java
@@ -12,58 +12,81 @@ import org.apache.flume.*;
 
 public class MyTwitterController {
 
-    private String consumerKey;
-    private String consumerSecret;
-    private String accessToken;
-    private String accessTokenSecret;
 
-    private String keywords;
+    MyTwitterController() {}
 
-    Twitter mytwitter = null;
 
-    MyTwitterController(Context context) {
-        initialize(context);
-    }
-
-    public void initialize(Context context) {
-
-        consumerKey = context.getString("consumerKey");
-        consumerSecret = context.getString("consumerSecret");
-        accessToken = context.getString("accessToken");
-        accessTokenSecret = context.getString("accessTokenSecret");
-
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setOAuthConsumerKey(consumerKey);
-        cb.setOAuthConsumerSecret(consumerSecret);
-        cb.setOAuthAccessToken(accessToken);
-        cb.setOAuthAccessTokenSecret(accessTokenSecret);
-        cb.setJSONStoreEnabled(true);
-        //cb.setIncludeEntitiesEnabled(true);
-        String keywordString = "#sarkodie";
-        keywords = keywordString.replace(",", "OR");
-
-        mytwitter = new TwitterFactory(cb.build()).getInstance();
-    }
-
-    public List<Status> fetchTweets() {
-        List<Status>allTweets = null;
+    public List<JSONObject> fetchTweets() {
+        List<JSONObject>allTweets = null;
 
         try {
-            Query query = new Query(keywords);
-            QueryResult result = null;
-            result = mytwitter.search(query);
 
-            allTweets = result.getTweets();
+            allTweets = this.dummyData();
 
-        }catch (TwitterException te) {
-            te.printStackTrace();
-            System.out.println("Failed to search tweets: " + te.getMessage());
-            System.exit(-1);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return allTweets;
     }
 
+    public List<JSONObject> dummyData(){
+
+        JSONObject jsonObject1 = new JSONObject();
+        jsonObject1.put("id",11833693);
+        jsonObject1.put("name","papa quecy");
+        jsonObject1.put("screenName","QuecyPapa");
+        jsonObject1.put("location","Koforidua, Ghana");
+        jsonObject1.put("reTweetCount",5);
+        jsonObject1.put("followersCount",6);
+        jsonObject1.put("isFollowRequestSent",false);
+        jsonObject1.put("profileImageUrl","http://pbs.twimg.com/profile_images/1184539643464900610/5buOSA1L_normal.jpg");
+        jsonObject1.put("isFollowRequestSent",false);
+        jsonObject1.put("isVerified",false);
+
+        JSONObject jsonObject2 = new JSONObject();
+        jsonObject2.put("id",11453693);
+        jsonObject2.put("name","Nana");
+        jsonObject2.put("screenName","Nana25893087");
+        jsonObject2.put("location","Kumasi, Ghana");
+        jsonObject2.put("reTweetCount",10);
+        jsonObject2.put("followersCount",30);
+        jsonObject2.put("isFollowRequestSent",false);
+        jsonObject2.put("profileImageUrl","http://pbs.twimg.com/profile_images/1202705788759883778/K0rb00B__normal.jpg");
+        jsonObject2.put("isFollowRequestSent",false);
+        jsonObject2.put("isVerified",false);
+
+        JSONObject jsonObject3 = new JSONObject();
+        jsonObject3.put("id",11476693);
+        jsonObject3.put("name","Agba Awo");
+        jsonObject3.put("screenName","Patoolee2");
+        jsonObject3.put("location","Accra, Ghana");
+        jsonObject3.put("reTweetCount",12);
+        jsonObject3.put("followersCount",150);
+        jsonObject3.put("isFollowRequestSent",false);
+        jsonObject3.put("profileImageUrl","http://pbs.twimg.com/profile_images/1174499821463461889/JY0WfvAH_normal.jpg");
+        jsonObject3.put("isFollowRequestSent",false);
+        jsonObject3.put("isVerified",true);
+
+
+        List data = Arrays.asList(
+                jsonObject1,
+                jsonObject1,
+                jsonObject1,
+                jsonObject1,
+                jsonObject1,
+                jsonObject2,
+                jsonObject2,
+                jsonObject2,
+                jsonObject2,
+                jsonObject2,
+                jsonObject3,
+                jsonObject3,
+                jsonObject3,
+                jsonObject3
+        );
+        return data;
+    }
+
 }
+
+
